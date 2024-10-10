6f4de6b1082b606a149aa28e8b8a6b3e3cb2a6ce
Ethan Cho <ethan.cho1826@gmail.com>
2024-10-10 12:05:28 -0500

    formatted code

--- list/main.cpp
+++ list/main.cpp
@@ -1,54 +1,52 @@
 #include <iostream>
+
 #include "ArrayList.h"
 #include "LinkedList.h"
 
 int main() {
-    ArrayList<int> list;
-    for(int i = 1; i <=5; i++) {
-        list.insert(i, i);
-    }
-    for(int i = 1; i <=5; i++) {
-        
-       std::cout << list.getEntry(i) << "\n";
-    }
+  ArrayList<int> list;
+  for (int i = 1; i <= 5; i++) {
+    list.insert(i, i);
+  }
+  for (int i = 1; i <= 5; i++) {
+    std::cout << list.getEntry(i) << "\n";
+  }
 
-    try {
-        std::cout << list.getEntry(6);
-    } catch (std::logic_error const& except) {
-        std::cout << "Failed to get position\n";
-    }
+  try {
+    std::cout << list.getEntry(6);
+  } catch (std::logic_error const& except) {
+    std::cout << "Failed to get position\n";
+  }
 
-    list.replace(3, 4);
-    std::cout << list.getEntry(3) << "\n";
+  list.replace(3, 4);
+  std::cout << list.getEntry(3) << "\n";
 
-    list.remove(3);
-    std::cout << list.getEntry(3) << "\n";
+  list.remove(3);
+  std::cout << list.getEntry(3) << "\n";
 
-    list.clear();
-    std::cout << list.getLength() << "\n";
+  list.clear();
+  std::cout << list.getLength() << "\n";
 
-    LinkedList<int> list2;
-    for(int i = 1; i <=5; i++) {
-        list2.insert(i, i);
-    }
-    for(int i = 1; i <=5; i++) {
-        
-       std::cout << list2.getEntry(i) << "\n";
-    }
+  LinkedList<int> list2;
+  for (int i = 1; i <= 5; i++) {
+    list2.insert(i, i);
+  }
+  for (int i = 1; i <= 5; i++) {
+    std::cout << list2.getEntry(i) << "\n";
+  }
 
-    try {
-        std::cout << list2.getEntry(6);
-    } catch (std::logic_error const& except) {
-        std::cout << "Failed to get position\n";
-    }
+  try {
+    std::cout << list2.getEntry(6);
+  } catch (std::logic_error const& except) {
+    std::cout << "Failed to get position\n";
+  }
 
-    list2.replace(3, 5);
-    std::cout << list2.getEntry(3) << "\n";
+  list2.replace(3, 5);
+  std::cout << list2.getEntry(3) << "\n";
 
-    list2.remove(3);
-    std::cout << list2.getEntry(3) << "\n";
+  list2.remove(3);
+  std::cout << list2.getEntry(3) << "\n";
 
-    list2.clear();
-    std::cout << list2.getLength() << "\n";
+  list2.clear();
+  std::cout << list2.getLength() << "\n";
 }
-
