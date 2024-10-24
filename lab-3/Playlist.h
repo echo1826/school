f91bbe8d160e78b80d67bab5b43644910d0e4a37
Ethan Cho <ethan.cho1826@gmail.com>
2024-10-24 18:26:06 -0500

    only need the header file

--- lab-3/Playlist.h
+++ lab-3/Playlist.h
@@ -9,22 +9,21 @@ class Playlist {
 		static const int DEFAULT_CAPACITY = 100;
 		int songCount;
 		int maxSongs;
-		int currentSongPosition;
-		ItemType songs[DEFAULT_CAPACITY];
-		ItemType history[DEFAULT_CAPACITY];
-		Mode currentMode;
+		int currentSongPosition; // keeps track of where in the playlist we are
+		ItemType songs[DEFAULT_CAPACITY]; // songs in playlist
+		ItemType history[DEFAULT_CAPACITY]; // history array that keeps track of what songs have been played
+		Mode currentMode; // the current mode the playlist is in
 	public:
 		Playlist();
-		void setMode(Mode newMode);
-		bool addSong(const ItemType& newSong);
-		bool removeSong(const ItemType& song);
-		void skip() const;
-		void pause() const;
-		void savePlaylist(const Playlist<ItemType>& playlist) const;
-		void shufflePlaylist();
-		void nextSong() const;
-		ItemType peekNextSong() const;
+		void setMode(Mode newMode); // switches the value of the current mode taking in the Mode enum value to switch to
+		bool addSong(const ItemType& newSong); // adds a new song to the songs array
+		bool removeSong(const ItemType& song); // takes a song input and tries to find and remove it from the existing songs array
+		ItemType skip() const; // skips the current song and returns the song it skipped to
+		void pause() const; // pauses the playlist
+		void savePlaylist(const Playlist<ItemType>& playlist) const; // ttakes a playlist input and writes it to a file to save the playlist
+		void shufflePlaylist(); // re-orders the song array
+		ItemType nextSong() const; // returns the next song that's to be played next
+		ItemType peekNextSong() const; // returns the next song in order
 };
 
-#include "Playlist.cpp"
 #endif
