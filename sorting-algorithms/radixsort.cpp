58c30c6beec4aef4e5eaba63998ae2986ed94664
Ethan Cho <ethan.cho1826@gmail.com>
2024-10-29 11:38:52 -0500

    radix sort psuedocode

--- sorting-algorithms/radixsort.cpp
+++ sorting-algorithms/radixsort.cpp
@@ -2,7 +2,17 @@
 
 template<class ItemType>
 void radixSort(ItemType arr[], int size, int digits) {
-    for(int i = digits; i >= 1; i--) {
+    for(int i = 0; i < digits; i++) {
         // create 10 buckets
+
+        // initialize a counter for each bucket to keep track of whats inside
+
+        // begin to sort the items into buckets
+        for(let j = 0; j < size; j++) {
+
+        }
+
+        // replace items in arr with items in each of the buckets starting with the first bucket
+        
     }
 }
