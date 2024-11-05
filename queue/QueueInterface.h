a7d83c5c3a3675859757cea9eaba3df0ba147e59
Ethan Cho <ethan.cho1826@gmail.com>
2024-11-05 10:16:41 -0600

    queue interface

---
+++ queue/QueueInterface.h
@@ -0,0 +1,15 @@
+#ifndef QUEUE_INTERFACE_
+#define QUEUE_INTEFACE
+
+template<class ItemType>
+class QueueInterface {
+    public:
+        virtual bool isEmpty() const = 0;
+        virtual bool enqueue(const ItemType& newEntry) = 0;
+        virtual bool dequeue() = 0;
+        virtual ItemType peekFront() const = 0;
+
+        virtual ~QueueInterface() {}
+};
+
+#endif
